08baa79ec489c1dbb4031577b22c6cedd921192b
Padma8962 <padmaja.sharma0073@gmail.com>
2025-06-12 11:22:40 +0000

    conversion

---
+++ 01_basics/03_conversionOfDataType.js
@@ -0,0 +1,15 @@
+let name = "padmaja"
+ console.log(typeof(Number(name)))
+
+ let name2 =Number("padmaja") 
+ console.log(name2) //not a number
+
+ let score = true
+ console.log(Number(score)) // 0=>false && 1=>true
+
+ let isLoggedin = 1
+ console.log(Boolean(isLoggedin))
+
+ let num = String(33)
+ console.log(num)
+ console.log(typeof(num))
