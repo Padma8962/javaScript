82991e3496bb2f043a73f4b58724d1abc53b4fac
Padma8962 <padmaja.sharma0073@gmail.com>
2025-06-12 10:57:47 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+let accountId = 123 
+var emailId ="padmaja.sharma0073@gmail.com"
+const empName ="padmaja" //const to define constant
+let empCity //undefined
+
+/*prefer not to use var because of issue in block scope and functional scope */
+/* bina keyword k bhi variable define kr sakte hai pr it is not a good practice */
+
+emailId ="mohitthakre8686@gmail.com"
+console.log(emailId)
+console.log([accountId,emailId,empName])
+console.table([accountId,emailId,empName,empCity])
---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version of js
+
+// alert(3+3) //we are using node.js not browser
+// code readablity should be high
+// ECMA is a documentation for javascript
+let name ="padmaja"
+let age = 23
+let isLoggedin = true
+
+// number
+// bigInt
+// boolean
+// string
+// null = standAlone
+// undefined
+// symbol=unique
+// object
+
+console.log(typeof(name))
+console.log(typeof(undefined))
+console.log(typeof(null))
