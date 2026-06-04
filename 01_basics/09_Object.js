1e331ca833c5faae25c9b47a4759b495d3a1a37c
Padma8962 <padmaja.sharma0073@gmail.com>
2026-06-04 11:49:45 +0000

    new update

--- 01_basics/09_Object.js
+++ 01_basics/09_Object.js
@@ -16,6 +16,12 @@ const jsUser = {
 jsUser.greeting = function(){
     console.log("hello")
 }
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(jsUser.greetingTwo())
 console.log(jsUser.greeting())
 //update krne k liye values ko overwrite kr sakte hai
 Object.freeze(jsUser) //ab update nhi kr sakte values ko
@@ -27,3 +33,64 @@ console.log(jsUser[mySym])
 
 //object constructor
 Object.create
+const tinderUser = new Object();
+// const tinderUser = {
+
+// }  same as the above object
+
+tinderUser.id="123456"
+tinderUser.name ="Padmaja"
+tinderUser.isLoggedin=false
+
+console.log(tinderUser);
+
+const regularUser ={
+    email : "Padmaja.sharma0073@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname :"Padmaja",
+            lastname : "Sharma"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1:"a" ,2:"b"};
+const obj2 = {3:"c" ,4:"d"};
+
+console.log({obj1,obj2});
+const obj3 = Object.assign({} ,obj1 ,obj2) //concatenate two objects inside {}
+// ...obj1 using . we are spreading elements of object
+
+// const obj3 ={...obj1 , ...obj2}
+// console.log(obj3)
+
+// values from database
+const users =[
+    {
+        id: "1",
+        email:"Rajesh.sharma0073@gmail.com"
+    },
+    {
+        id: "2",
+        email:"Sarika.sharma0073@gmail.com"
+    },
+    {
+        id: "3",
+        email:"Padmaja.sharma0073@gmail.com"
+    },
+    {
+        id: "4",
+        email:"AshuTosh.sharma0073@gmail.com"
+    }
+]
+
+console.log(users[1].email); //Output : Rajesh.sharma0073@gmail.com
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.llog(tinderUser.hasOwnProperty('isLogged'));
+
