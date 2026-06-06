c8efc9976f42fe8611991bce48ec5854c5709ddf
Padma8962 <padmaja.sharma0073@gmail.com>
2026-06-06 05:35:38 +0000

    JSON formatting

--- 01_basics/11_DestructuringObj_and_JSON_API.js
+++ 01_basics/11_DestructuringObj_and_JSON_API.js
@@ -1,3 +1,4 @@
+// {} mtlb destructuring
 const course = {
     coursename :"Java Script",
     price : "999",
@@ -9,3 +10,35 @@ console.log(courseInstructor)
 
 const {courseInstructor : instructor } =cousre //alias name : instructor
 console.log(instructor)
+
+const navbar = ({company}) =>{
+
+}
+
+
+navbar(company = "PnM")
+
+// ++++++++++++++++++++API++++++++++++++++++++
+
+// API k through phle valuses XML m aati thi pr ab JSON m aati hai
+// below is the JSON format
+// {
+//     "name" : "Padmaja",
+//     "coursename" : "js in hindi",
+//     "price" : "free"
+// }
+
+// below is in array format
+// [
+//     {
+        
+//     },
+//     {
+
+//     },
+//     {
+
+//     }
+// ]
+
+// JSON formatter
