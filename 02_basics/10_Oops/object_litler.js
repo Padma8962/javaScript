4d7a79296c55e87973e2cb7b243dc29ca2226918
Padma8962 <padmaja.sharma0073@gmail.com>
2026-06-24 07:21:17 +0000

    object litler

---
+++ 02_basics/10_Oops/object_litler.js
@@ -0,0 +1,38 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+// new keyword is a constructor function which is used to create an instance of an object. It is used to create a new object and set the value of this keyword to the new object. It also sets the prototype of the new object to the prototype of the constructor function. It also returns the new object. 
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
